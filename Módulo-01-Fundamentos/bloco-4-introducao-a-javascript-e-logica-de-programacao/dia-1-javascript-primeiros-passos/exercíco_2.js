210c925b8bf5488f6ebcccc746d37232d14520cb
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-02-04 14:22:43 -0300

    Adiciona mais excercícios

---
+++ Módulo-01-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercíco_2.js
@@ -0,0 +1,10 @@
+// 2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const a = 45;
+const b = 23;
+
+if (a > b) {
+  console.log("'a' é maior que 'b'");
+} else {
+  console.log("'b' é maior que 'a'");
+};
